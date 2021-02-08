ba7a6f88a69cd8a6ac31ae84516c5c1ec1877241
Jaime Ironhacker <jaimejuradoperez@gmail.com>
2021-02-08 17:41:28 +0100

    changes routes for merge

--- config/routes.config.js
+++ config/routes.config.js
@@ -4,16 +4,13 @@ const productController = require('../controllers/product.controller')
 const userController = require('../controllers/user.controller')
 const Product = require('../models/product.model')
 const secure = require('../middlewares/secure.middlewares')
-const cartController = require('../controllers/cart.controller')
-//nuevo
 const GOOGLE_OAUTH_SCOPES = ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'];
 const passport = require('passport');
-
 router.get('/', (req, res, next) => {
     Product.find()
         .then(products => {
-            if (products){
-                res.render('home', {products});
+            if (products) {
+                res.render('home', { products });
             } else {
                 res.render('home');
             }
@@ -21,31 +18,17 @@ router.get('/', (req, res, next) => {
         .catch(error => {
             console.error('no carga la lista de productos');
             res.redirect('/')
-        })  
+        })
 });
-
-//no estoy seguro si la ruta aqui es la correcta
-router.get('/activate',secure.isAuthenticated, userController.activate);
-//falta secure autenticade en rutas por bugs
+router.get('/activate', secure.isAuthenticated, userController.activate);
 router.get('/users/register', userController.register);
 router.post('/users/register', userController.doRegister);
-router.get('/users/login', userController.login);
 router.post('/users/login', userController.doLogin);
-router.get('/users/profile/:id',secure.isAuthenticated, userController.profile);
-
-
+router.get('/users/login', userController.login);
+router.get('/users/profile/:id', secure.isAuthenticated, userController.profile);
 router.get('/products/register', secure.isAuthenticated, productController.register);
 router.post('/products/register', secure.isAuthenticated, productController.createProduct);
-router.get('/products/edit/:id', secure.isAuthenticated, productController.edit)
-router.post('/products/edit/:id', secure.isAuthenticated, productController.doEdit)
-router.get('/products/list/:id', secure.isAuthenticated, productController.list)
-//nuevo
 router.get('/authentication/google', passport.authenticate('google-auth', { scope: GOOGLE_OAUTH_SCOPES }));
 router.get('/authentication/google/cb', userController.loginWithGoogle);
-
 router.post('/users/logout', secure.isAuthenticated, userController.logout);
-
-router.get('/cart', secure.isAuthenticated, cartController.cart);
-
 module.exports = router;
-
