a51a4c357f38129af78b8d6e16d85ed3c465fb9c
Jaime Ironhacker <jaimejuradoperez@gmail.com>
2021-02-22 19:22:26 +0100

    test heroku 3

--- controllers/common.controller.js
+++ controllers/common.controller.js
@@ -4,30 +4,32 @@ const Product = require('../models/product.model')
 const User = require('../models/user.model')
 
 module.exports.home = (req, res, next) => {
-    Product.find()
-        .then(products => {
-            if (products) {
-                User.find()
-                    .then(users => {
-                        let productsLimit = products.slice(0, 3)
-                        let usersSortList = users.slice(0, 3);
-                        res.render('home', { 
-                            products: productsLimit,
-                            users: usersSortList
-                        });
-                    })
-                    .catch(error => {
-                        console.log(error)
-                        next()
-                    })
-            } else {
-                next()
-            }
-        })
-        .catch(error => {
-            console.error('no carga la lista de productos');
-            res.redirect('/')
-        })
+   // Product.find()
+   //     .then(products => {
+   //         if (products) {
+   //             User.find()
+   //                 .then(users => {
+   //                     let productsLimit = products.slice(0, 3)
+   //                     let usersSortList = users.slice(0, 3);
+   //                     res.render('home', { 
+   //                         products: productsLimit,
+   //                         users: usersSortList
+   //                     });
+   //                 })
+   //                 .catch(error => {
+   //                     console.log(error)
+   //                     next()
+   //                 })
+   //         } else {
+   //             next()
+   //         }
+   //     })
+   //     .catch(error => {
+   //         console.error('no carga la lista de productos');
+   //         res.redirect('/')
+   //     })
+
+   res.render('home')
 }
 
 module.exports.about = (req, res,next) => {
